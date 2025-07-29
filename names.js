16d3b69c3ba18a77af71b7ff56bf08050adae8ce
anthony-murè <tony.mure99@gmail.com>
2025-07-29 13:22:54 +0200

    first commit

---
+++ names.js
@@ -0,0 +1,3 @@
+/*1 - names.js
+Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+Esporta la funzione dal file */
