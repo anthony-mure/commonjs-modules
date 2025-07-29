8fb2f9a66f27659298eabe422e37a4da5cb410ac
anthony-murè <tony.mure99@gmail.com>
2025-07-29 16:58:19 +0200

    [add]exercise-1/names.js

--- names.js
+++ names.js
@@ -1,3 +1,14 @@
 /*1 - names.js
 Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 Esporta la funzione dal file */
+
+
+const fullName = (firstName, lastName) => {
+  return {
+    firstName ,
+    lastName 
+  };
+};
+
+
+module.exports = fullName;
--- people.js
+++ people.js
@@ -2,3 +2,6 @@
 Importa la tua funzione da names.js
 Importa la tua funzione da hobbies.js
 Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.*/
+
+const fullName = require('./names');
+
